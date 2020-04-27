ed12119e8b8209865f21d6044c6b106b62b76a70
Aleksey Razbakov <aleksey@razbakov.com>
2020-04-27 19:36:11 +0200

    fix date field

--- components/TField.vue
+++ components/TField.vue
@@ -25,6 +25,7 @@
           ref="input"
           v-bind="$attrs"
           autocomplete="off"
+          :type="type"
           :value.sync="value"
           :class="inputClasses"
           class="w-full block bg-gray-200 appearance-none font-mono border-2 border-gray-200 rounded py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
